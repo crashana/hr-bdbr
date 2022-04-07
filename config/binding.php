6f636ae672ff62176891030dfa8254a0e54558b3
givisharabidze <givisharabidze@gmail.com>
2022-04-07 18:46:01 +0400

    created config file for repository bindings

--- config/binding.php
+++ config/binding.php
@@ -2,29 +2,10 @@
 
 return [
 
-    [
+    /*[
         'interface' => App\Repositories\Admins\AdminRepoInterface::class,
         'cache' => App\Repositories\Admins\AdminCacheRepo::class,
         'direct' => App\Repositories\Admins\AdminRepo::class,
     ],
-
-    [
-        'interface' => App\Repositories\Admins\Roles\RoleRepoInterface::class,
-        'cache' => App\Repositories\Admins\Roles\RoleRepo::class,
-        'direct' => App\Repositories\Admins\Roles\RoleRepo::class
-    ],
-
-    [
-        'interface' => App\Repositories\Locales\LocalesRepoInterface::class,
-        'cache' => App\Repositories\Locales\LocalesCacheRepo::class,
-        'direct' => App\Repositories\Locales\LocalesRepo::class
-    ],
-
-
-    [
-        'interface' => App\Repositories\Media\MediaRepoInterface::class,
-        'cache' => App\Repositories\Media\MediaRepo::class,
-        'direct' => App\Repositories\Media\MediaRepo::class
-    ],
-
+    */
 ];
